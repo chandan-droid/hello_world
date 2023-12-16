9a121412d7bea617afa2958eb1cbb141984e7063
dev <147903617+chandan-droid@users.noreply.github.com>
2023-12-16 15:01:49 +0530

    app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(
-  MaterialApp(
-    title: "Hello World",
-    home: Material(
-      color: Colors.white,
-      child:  Center(
-        child:Text("Hello World",
-
-
+ runApp(
+     MaterialApp(
+      home: Scaffold(
+        backgroundColor:Colors.white24,
+        appBar:AppBar(title: const Text("Home page",),
+               backgroundColor:Colors.grey,
+               elevation: 20.0,
+               shadowColor: Colors.black87 ,
         ),
-
+        body: const Center(
+          child: Text('Hello World',
+            style: TextStyle(
+                color: Colors.tealAccent,
+                fontSize: 30,
+                fontWeight:FontWeight.bold
+            ),
+          ),
+        ),
+      ),
     )
-  )
-)
-);
+ );
 }
 
