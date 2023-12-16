d4759c8b7aa08638e2f9bc92dc097063470fa8bc
dev <147903617+chandan-droid@users.noreply.github.com>
2023-12-16 13:59:03 +0530

    app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+runApp(
+  MaterialApp(
+    title: "Hello World",
+    home: Material(
+      color: Colors.white,
+      child:  Center(
+        child:Text("Hello World",
+
+
+        ),
+
+    )
+  )
+)
+);
+}
+
